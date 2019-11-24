acfc7816b5f92f7bc45bea9efe799750a3b93995
czxcaroline <czx@seas.upenn.edu>
2019-11-24 13:09:55 -0500

    Updates

---
+++ precache-manifest.3662ef68252291c9ef5f326c29e68ead.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "833e2d8114cec4a2e5da33a22ca444f4",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e07f90d3e04ae8708629",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "12580d20d0a46c9b2f9f",
+    "url": "/robofriends/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e07f90d3e04ae8708629",
+    "url": "/robofriends/static/js/2.7514f082.chunk.js"
+  },
+  {
+    "revision": "12580d20d0a46c9b2f9f",
+    "url": "/robofriends/static/js/main.459ba83c.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  }
+]);
